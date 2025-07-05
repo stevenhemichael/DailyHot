2c96d68e951e1f69dcdcf21100028af9ec875cfa
stevenhemichael <135568733+stevenhemichael@users.noreply.github.com>
2025-07-05 16:03:05 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -9,83 +9,86 @@ export const mainStore = defineStore("mainData", {
       // 新闻类别
       defaultNewsArr: [
         {
-          label: "哔哩哔哩",
-          name: "bilibili",
-          order: 0,
-          show: true,
-        },
-        {
-          label: "微博",
-          name: "weibo",
-          order: 1,
-          show: true,
-        },
-        {
-          label: "抖音",
-          name: "douyin",
-          order: 2,
-          show: true,
-        },
-        {
-          label: "知乎",
-          name: "zhihu",
-          order: 3,
-          show: true,
-        },
-        {
-          label: "36氪",
-          name: "36kr",
-          order: 4,
-          show: true,
-        },
-        {
           label: "百度",
           name: "baidu",
-          order: 5,
-          show: true,
-        },
-        {
-          label: "少数派",
-          name: "sspai",
-          order: 6,
-          show: true,
-        },
-        {
-          label: "IT之家",
-          name: "ithome",
-          order: 7,
-          show: true,
-        },
-        {
-          label: "澎湃新闻",
-          name: "thepaper",
-          order: 8,
+          order: 0,
           show: true,
         },
         {
           label: "今日头条",
           name: "toutiao",
-          order: 9,
+          order: 1,
           show: true,
         },
         {
+          label: "IT之家",
+          name: "ithome",
+          order: 2,
+          show: true,
+        },
+        {
+          label: "微博",
+          name: "weibo",
+          order: 3,
+          show: true,
+        },
+        {
+          label: "抖音",
+          name: "douyin",
+          order: 4,
+          show: true,
+        },
+        {
+          label: "少数派",
+          name: "sspai",
+          order: 5,
+          show: true,
+        },
+        {
+          label: "澎湃新闻",
+          name: "thepaper",
+          order: 6,
+          show: true,
+        },
+         {
+          label: "腾讯新闻",
+          name: "qq-news",
+          order: 7,
+          show: true,
+        },
+        {
+          label: "36氪",
+          name: "36kr",
+          order: 8,
+          show: true,
+        },
+        {
+          label: "哔哩哔哩",
+          name: "bilibili",
+          order: 9,
+          show: true,
+        },
+       {
+          label: "网易新闻",
+          name: "netease-news",
+          order: 10,
+          show: true,
+        },
+        
+        
+        {
           label: "百度贴吧",
           name: "tieba",
-          order: 10,
+          order: 11,
           show: true,
         },
         {
           label: "稀土掘金",
           name: "juejin",
-          order: 11,
-          show: true,
-        },
-        {
-          label: "腾讯新闻",
-          name: "qq-news",
           order: 12,
           show: true,
         },
+       
         {
           label: "豆瓣电影",
           name: "douban-movie",
@@ -101,42 +104,43 @@ export const mainStore = defineStore("mainData", {
         {
           label: "崩坏：星穹铁道",
           name: "starrail",
-          order: 16,
+          order: 15,
           show: true,
         },
         {
           label: "LOL",
           name: "lol",
-          order: 15,
+          order: 16,
           show: true,
         },
-        {
-          label: "网易新闻",
-          name: "netease-news",
-          order: 17,
-          show: true,
-        },
+        
         {
           label: "微信读书",
           name: "weread",
-          order: 18,
+          order: 17,
           show: true,
         },
         {
           label: "豆瓣讨论小组",
           name: "douban-group",
-          order: 19,
+          order: 18,
           show: true,
         },
         {
           label: "NGA",
           name: "ngabbs",
-          order: 20,
+          order: 19,
           show: true,
         },
         {
           label: "HelloGitHub",
           name: "hellogithub",
+          order: 20,
+          show: true,
+        },
+         {
+          label: "知乎",
+          name: "zhihu",
           order: 21,
           show: true,
         },
@@ -161,7 +165,7 @@ export const mainStore = defineStore("mainData", {
       // 时间数据
       timeData: null,
       // 字体大小
-      listFontSize: 16,
+      listFontSize: 10,
     };
   },
   getters: {},
